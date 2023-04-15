4675c23315e6b5b276196c0b05ee2718e16246c2
EvgeniyKazancev <ekazancev777@gmail.com>
2023-04-15 20:45:50 +0300

    Новый коммит

---
+++ src/main/java/QuizCardBuilder.java
@@ -0,0 +1,121 @@
+import javax.management.JMException;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.ArrayList;
+
+public class QuizCardBuilder {
+    private JTextArea question;
+    private JTextArea answer;
+    private ArrayList<QuizCard> cardList;
+    private JFrame frame;
+
+    public static void main(String[] args) {
+        QuizCardBuilder builder = new QuizCardBuilder();
+        builder.go();
+    }
+
+    public void go() {
+        frame = new JFrame("QuizCardBuilder");
+        JPanel mainPanel = new JPanel();
+        Font bigFont = new Font("sanserif", Font.BOLD, 24);
+        question = new JTextArea(6, 20);
+        question.setLineWrap(true);
+        question.setWrapStyleWord(true);
+        question.setFont(bigFont);
+
+        JScrollPane qScroller = new JScrollPane(question);
+        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        answer = new JTextArea(6, 20);
+        answer.setWrapStyleWord(true);
+        answer.setLineWrap(true);
+        answer.setFont(bigFont);
+
+        JScrollPane aScroller = new JScrollPane();
+        aScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        aScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        JButton nextButton = new JButton("Next Card");
+
+        cardList = new ArrayList<QuizCard>();
+
+        JLabel qLabel = new JLabel("Question:");
+        JLabel aLabel = new JLabel("Answer");
+
+        mainPanel.add(qLabel);
+        mainPanel.add(qScroller);
+        mainPanel.add(aLabel);
+        mainPanel.add(aScroller);
+        mainPanel.add(nextButton);
+        nextButton.addActionListener(new NextCardListener());
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem newMenuItem = new JMenuItem("New");
+
+        JMenuItem saveMenuItem = new JMenuItem("Save");                 //  Мы создаем обьект
+        newMenuItem.addActionListener(new NewMenuListener());                //Jframe  и добавляем в него меню File и Save
+        saveMenuItem.addActionListener(new SaveMenuListener());              //Затем говорим главной панели что она должна использовать JMenuBar
+        fileMenu.add(newMenuItem);                                           //Пункты меню могут запускать ActionEvent
+        fileMenu.add(saveMenuItem);                                          //
+        menuBar.add(fileMenu);                                                //
+        frame.setJMenuBar(menuBar);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(500, 600);
+        frame.setVisible(true);
+
+
+    }
+
+    private class NextCardListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            QuizCard card = new QuizCard(question.getText() < answer.getText());
+            cardList.add(card);
+        }
+    }
+
+    private class SaveMenuListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            QuizCard card = new QuizCard(question.getText() < answer.getText());
+            cardList.add(card);
+
+            JFileChooser fileSave = new JFileChooser();   //Вызывается диалоговое окно и программа останавливается на этой строке, пока
+            fileSave.showSaveDialog(frame);               //пользователь не выберет меню  Save. Всю навигацию выбор файла за вас выполняет класс JFileChooser!
+            saveFile(fileSave.getSelectedFile());         //
+        }
+    }
+
+    private class NewMenuListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            cardList.clear();
+            clearCard();
+        }
+    }
+
+    private void clearCard() {
+        question.setText("");
+        answer.setText("");
+        question.requestFocus();
+    }
+
+    private void saveFile(File file) {
+       try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            for (QuizCard card : cardList) {
+            writer.write(card.);// ДОПИСАТЬ
+             writer.write(card.);
+            }
+       writer.close();
+       } catch (IOException ex){
+           System.out.println("couldn write the cardList out");
+           ex.printStackTrace();
+       }
+    }
+}
